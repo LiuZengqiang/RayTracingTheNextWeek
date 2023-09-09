ccc6f0dc08656b951bce1d24571afc46ea26ebc1
liuzengqiang <12021032@zju.edu.cn>
2023-09-09 17:18:49 +0800

    feat:增加image纹理

--- include/texture.h
+++ include/texture.h
@@ -11,7 +11,9 @@
 #ifndef TEXTURE_H
 #define TEXTURE_H
 
+#include "rtw_stb_image.h"
 #include "rtweekend.h"
+
 // 纹理类
 class texture {
  public:
@@ -65,5 +67,34 @@ class checker_texture : public texture {
   double inv_scale;
   shared_ptr<texture> even;
   shared_ptr<texture> odd;
+};
+
+// 图片纹理类
+class image_texture : public texture {
+ public:
+  image_texture(const char* filename) : image(filename) {}
+
+  // 获取图片 image 在二维局部坐标(u,v)下的颜色值
+  color value(double u, double v, const point3& p) const override {
+    // If we have no texture data, then return solid cyan as a debugging aid.
+    if (image.height() <= 0) return color(0, 1, 1);
+
+    // Clamp input texture coordinates to [0,1] x [1,0]
+
+    // 计算(u,v)坐标
+    u = interval(0, 1).clamp(u);
+    v = 1.0 - interval(0, 1).clamp(v);  // Flip V to image coordinates
+
+    auto i = static_cast<int>(u * image.width());
+    auto j = static_cast<int>(v * image.height());
+    auto pixel = image.pixel_data(i, j);
+
+    auto color_scale = 1.0 / 255.0;
+    return color(color_scale * pixel[0], color_scale * pixel[1],
+                 color_scale * pixel[2]);
+  }
+
+ private:
+  rtw_image image;
 };
 #endif
--- main.cpp
+++ main.cpp
@@ -119,7 +119,30 @@ void two_spheres() {
 
   cam.render(world);
 }
+void earth() {
+  auto earth_texture = make_shared<image_texture>("earthmap.jpg");
+
+  auto earth_surface = make_shared<lambertian>(earth_texture);
+  auto globe = make_shared<sphere>(point3(0, 0, 0), 2, earth_surface);
+
+  camera cam;
+
+  cam.aspect_ratio = 16.0 / 9.0;
+  cam.image_width = 400;
+  cam.samples_per_pixel = 100;
+  cam.max_depth = 50;
+
+  cam.vfov = 20;
+  // 这里的 lookfrom 应该为 (12,0,0) 才能和网站上的结果相同
+  cam.lookfrom = point3(12, 0, 0);
+  cam.lookat = point3(0, 0, 0);
+  cam.vup = vec3(0, 1, 0);
+
+  cam.defocus_angle = 0;
+
+  cam.render(hittable_list(globe));
+}
 int main() {
-  two_spheres();
+  earth();
   return 0;
 }
